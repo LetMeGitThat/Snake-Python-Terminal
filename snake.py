efa73a40509844c2af3198a4d0b2386618dd0d88
GItThat <xtraterrestri@gmail.com>
2024-06-03 18:44:35 -0400

    Apples and optimizations
    You can eat apples and did some optimizations. Also unoptimized it with these globals but whatever

--- snake.py
+++ snake.py
@@ -6,30 +6,21 @@ import threading
 
 BOARD_WIDTH = 10
 BOARD_HEIGHT = 10
+UPDATE_SPEED = 0.1
 
-snakeCharacter = "S"
-appleCharacter = "O"
+SNAKE_CHARACTER = "S"
+APPLE_CHARACTER = "O"
 
 snake = [(0, 2)]
 xVel = 1
 yVel = 0
-display = [
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"]
-]
+display = [[" " for _ in range(BOARD_WIDTH)] for _ in range(BOARD_HEIGHT)]
 
-updateSnake = False
+grow = False
 running = True
 
 def input():
+    # handle input
     global xVel
     global yVel
 
@@ -52,34 +43,64 @@ def input():
             return
 
 def update_snake():
+    global grow
     # add an element to the first position where the head should be and pop the table
     position = (snake[0][0]+xVel, snake[0][1]+yVel)
     if position[0] >= BOARD_WIDTH or position[1] >= BOARD_HEIGHT or position[0] < 0 or position[1] < 0:
-        print("player has hit a wall end it") 
         return False
     snake.insert(0, position)
 
-def update_display():
+    # do not pop if we eat an apple
+    if not grow:
+        snake.pop()
+    grow = False
+
+def update_display(applePosition):
+    # display the apple
+    display[applePosition[1]][applePosition[0]] = APPLE_CHARACTER
+
+    # display the snake
     for position in snake:
-        display[position[1]][position[0]] = snakeCharacter
+        display[position[1]][position[0]] = SNAKE_CHARACTER
 
 def show_display():
+    # show the display with a buffer
     for j in range(BOARD_HEIGHT):
         buffer = ""
         for i in range(BOARD_WIDTH):
             buffer += display[j][i]
         print(buffer)
 
-def main():
+def place_apple():
+    x = random.randint(0, BOARD_WIDTH-1)
+    y = random.randint(0, BOARD_HEIGHT-1)
+    position = (x, y)
+    return position
+
+
+def start():
+    global display
+    global grow
     inputThread = threading.Thread(target=input)
     inputThread.start()
 
+    applePosition = place_apple()
+
     while running:
+        if snake[0][0] == applePosition[0] and snake[0][1] == applePosition[1]:
+            grow = True
+            applePosition = place_apple()
+        
         update_snake()
-        update_display()
+        update_display(applePosition)
         show_display()
-        time.sleep(.5)
+
+        time.sleep(UPDATE_SPEED)
+
+        display = [[" " for _ in range(BOARD_WIDTH)] for _ in range(BOARD_HEIGHT)]
         os.system("cls")
         
+        
+        
 
-main()
+start()
